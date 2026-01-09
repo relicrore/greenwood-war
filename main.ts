981bc60a289cb19fcc53f14af8da52274ebe458f
EugeneRedwing8 <emersonejohnson@gmail.com>
2026-01-09 13:46:20 -0800

    Restore '12/8/2025, 11:59:34 AM 1.0.5'

--- main.ts
+++ main.ts
@@ -39,6 +39,159 @@ statusbars.onStatusReached(StatusBarKind.EnemyHealth, statusbars.StatusCompariso
     }
     KILLS += 1
 })
+function LevelSetup (Level: number) {
+    SwapSong()
+    Reset()
+    if (Level == 0) {
+        PineconeNumber = 50
+        MISSION = 1
+        scroller.setLayerImage(scroller.BackgroundLayer.Layer0, assets.image`Pale Hail Forest layer 0`)
+        scroller.scrollBackgroundWithSpeed(-5, 0, scroller.BackgroundLayer.Layer0)
+        scroller.setLayerImage(scroller.BackgroundLayer.Layer1, assets.image`Pale Hail Forest layer 1`)
+        scroller.setCameraScrollingMultipliers(0.1, 0, scroller.BackgroundLayer.Layer1)
+        scroller.setLayerImage(scroller.BackgroundLayer.Layer2, assets.image`Pale Hail Forest layer 2`)
+        scroller.setCameraScrollingMultipliers(0.25, 0, scroller.BackgroundLayer.Layer2)
+        tiles.setCurrentTilemap(tilemap`PHFm1`)
+        VisualTileMapLayers.addVisualTileMapLayer(tilemap`PHFm1FG`, 100)
+        Hailing = true
+        MoveAbility = true
+        SongStopped = false
+        Cold_Hearted_Pale_Hail_Forest()
+    } else if (Level == 1) {
+        PineconeNumber = 20
+        MISSION = 2
+        scroller.setLayerImage(scroller.BackgroundLayer.Layer0, assets.image`Pale Hail Forest layer 0`)
+        scroller.scrollBackgroundWithSpeed(-5, 0, scroller.BackgroundLayer.Layer0)
+        scroller.setLayerImage(scroller.BackgroundLayer.Layer1, assets.image`Pale Hail Forest layer 1`)
+        scroller.setCameraScrollingMultipliers(0.1, 0, scroller.BackgroundLayer.Layer1)
+        scroller.setLayerImage(scroller.BackgroundLayer.Layer2, assets.image`Pale Hail Forest layer 2`)
+        scroller.setCameraScrollingMultipliers(0.25, 0, scroller.BackgroundLayer.Layer2)
+        VisualTileMapLayers.addVisualTileMapLayer(tilemap`PHFm2Cutscene`, -10)
+        tiles.setCurrentTilemap(tilemap`PHFm2`)
+    } else if (Level == 2) {
+        PineconeNumber = 20
+        MISSION = 0
+        scroller.setLayerImage(scroller.BackgroundLayer.Layer0, assets.image`Pale Hail Forest layer 0`)
+        scroller.scrollBackgroundWithSpeed(-5, 0, scroller.BackgroundLayer.Layer0)
+        scroller.setLayerImage(scroller.BackgroundLayer.Layer1, assets.image`Pale Hail Forest layer 1`)
+        scroller.setCameraScrollingMultipliers(0.1, 0, scroller.BackgroundLayer.Layer1)
+        scroller.setLayerImage(scroller.BackgroundLayer.Layer2, assets.image`Pale Hail Forest layer 2`)
+        scroller.setCameraScrollingMultipliers(0.25, 0, scroller.BackgroundLayer.Layer2)
+        VisualTileMapLayers.addVisualTileMapLayer(tilemap`PHFm3BG`, -100)
+        tiles.setCurrentTilemap(tilemap`PHFm3`)
+        Hailing = true
+        MoveAbility = true
+        SongStopped = false
+        Cold_Hearted_Pale_Hail_Forest()
+    } else if (Level == 3) {
+        PineconeNumber = 20
+        MISSION = 4
+        scroller.setLayerImage(scroller.BackgroundLayer.Layer0, assets.image`Nitro Stun layer 0`)
+        VisualTileMapLayers.addVisualTileMapLayer(tilemap`NSm4BG`, -100)
+        tiles.setCurrentTilemap(tilemap`NSm4`)
+        VisualTileMapLayers.addVisualTileMapLayer(tilemap`NSm4FG`, 100)
+    } else if (Level == 4) {
+        PineconeNumber = 20
+        MISSION = 5
+        scroller.setLayerImage(scroller.BackgroundLayer.Layer0, assets.image`Nitro Stun layer 0`)
+        VisualTileMapLayers.addVisualTileMapLayer(tilemap`NSm5BG`, -100)
+        tiles.setCurrentTilemap(tilemap`NSm5`)
+    } else if (Level == 5) {
+        PineconeNumber = 20
+        MISSION = 6
+        scroller.setLayerImage(scroller.BackgroundLayer.Layer0, assets.image`Nitro Stun layer 0`)
+        tiles.setCurrentTilemap(tilemap`NSm6`)
+    } else if (Level == 6) {
+        PineconeNumber = 20
+        MISSION = 7
+        scroller.setLayerImage(scroller.BackgroundLayer.Layer0, assets.image`Magma Assault layer 0`)
+        scroller.setCameraScrollingMultipliers(0.2, 0, scroller.BackgroundLayer.Layer0)
+        tiles.setCurrentTilemap(tilemap`MAm7`)
+        VisualTileMapLayers.addVisualTileMapLayer(tilemap`MAm7FG`, 100)
+    } else {
+    	
+    }
+    Aquifer = sprites.create(assets.image`WaterHitbox`, SpriteKind.AquiferImage)
+    Aquifer.setFlag(SpriteFlag.GhostThroughWalls, true)
+    PlayerHitbox = sprites.create(assets.image`WaterHitbox`, SpriteKind.Player)
+    PlayerHitbox.setFlag(SpriteFlag.Invisible, true)
+    Aquifer.z = 50
+    basics.add_gravity_to(PlayerHitbox)
+    scene.cameraFollowSprite(PlayerHitbox)
+    tiles.placeOnRandomTile(PlayerHitbox, assets.tile`START`)
+    tiles.setTileAt(PlayerHitbox.tilemapLocation(), assets.tile`transparency16`)
+    AquiferAnims()
+    CUTSCENE()
+    timer.after(30, function () {
+        for (let value of tiles.getTilesByType(assets.tile`WaterSwordsmanSpawner`)) {
+            WaterHitbox = sprites.create(assets.image`WaterHitbox`, SpriteKind.AllyHitbox)
+            WaterHitbox.setFlag(SpriteFlag.Invisible, true)
+            WaterNPC = sprites.create(assets.image`WaterHitbox`, SpriteKind.Ally)
+            sprites.setDataSprite(WaterHitbox, "image", WaterNPC)
+            WaterNPC.setFlag(SpriteFlag.GhostThroughWalls, true)
+            tiles.placeOnTile(WaterHitbox, value)
+            tiles.setTileAt(value, assets.tile`transparency16`)
+            basics.add_gravity_to(WaterHitbox)
+        }
+        WaterAnims()
+        pauseUntil(() => MoveAbility)
+        for (let value2 of tiles.getTilesByType(assets.tile`OilSwordsmanSpawner`)) {
+            OilHitbox = sprites.create(assets.image`OilHitbox`, SpriteKind.EnemyHitbox)
+            sprites.setDataNumber(OilHitbox, "SpawnX", value2.x)
+            sprites.setDataNumber(OilHitbox, "SpawnY", value2.y)
+            OilHitbox.setFlag(SpriteFlag.Invisible, true)
+            OilNPC = sprites.create(assets.image`OilHitbox`, SpriteKind.Enemy)
+            sprites.setDataSprite(OilHitbox, "image", OilNPC)
+            OilNPC.setFlag(SpriteFlag.GhostThroughWalls, true)
+            tiles.placeOnTile(OilHitbox, value2)
+            tiles.setTileAt(value2, assets.tile`transparency16`)
+            basics.add_gravity_to(OilHitbox)
+            OilHealth = statusbars.create(15, 4, StatusBarKind.EnemyHealth)
+            OilHealth.setColor(12, 15)
+            OilHealth.setBarBorder(1, 15)
+            OilHealth.max = 2
+            OilHealth.attachToSprite(OilHitbox)
+        }
+        for (let value22 of tiles.getTilesByType(assets.tile`OilRangerSpawner`)) {
+            OilHitbox = sprites.create(assets.image`OilHitbox`, SpriteKind.EnemyRHitbox)
+            sprites.setDataNumber(OilHitbox, "SpawnX", value22.x)
+            sprites.setDataNumber(OilHitbox, "SpawnY", value22.y)
+            OilHitbox.setFlag(SpriteFlag.Invisible, true)
+            OilNPC = sprites.create(assets.image`OilHitbox`, SpriteKind.Enemy)
+            sprites.setDataSprite(OilHitbox, "image", OilNPC)
+            OilNPC.setFlag(SpriteFlag.GhostThroughWalls, true)
+            tiles.placeOnTile(OilHitbox, value22)
+            tiles.setTileAt(value22, assets.tile`transparency16`)
+            basics.add_gravity_to(OilHitbox)
+            OilHealth = statusbars.create(15, 4, StatusBarKind.EnemyHealth)
+            OilHealth.setColor(14, 15)
+            OilHealth.setBarBorder(1, 15)
+            OilHealth.max = 2
+            OilHealth.attachToSprite(OilHitbox)
+        }
+        for (let value3 of tiles.getTilesByType(assets.tile`OilSurgingSpawner`)) {
+            OilHitbox = sprites.create(assets.image`OilHitbox`, SpriteKind.EnemySrHitbox)
+            sprites.setDataNumber(OilHitbox, "SpawnX", value3.x)
+            sprites.setDataNumber(OilHitbox, "SpawnY", value3.y)
+            OilHitbox.setFlag(SpriteFlag.Invisible, true)
+            OilNPC = sprites.create(assets.image`OilHitbox`, SpriteKind.Enemy)
+            sprites.setDataSprite(OilHitbox, "image", OilNPC)
+            OilNPC.setFlag(SpriteFlag.GhostThroughWalls, true)
+            tiles.placeOnTile(OilHitbox, value3)
+            tiles.setTileAt(value3, assets.tile`transparency16`)
+            basics.add_gravity_to(OilHitbox)
+            OilHealth = statusbars.create(15, 4, StatusBarKind.EnemyHealth)
+            OilHealth.setColor(8, 15)
+            OilHealth.setBarBorder(1, 15)
+            OilHealth.max = 1
+            OilHealth.attachToSprite(OilHitbox)
+        }
+        OilAnims()
+        SetUpHUD()
+        SetUpOilNum()
+        KILLS = 0
+    })
+}
 scene.onOverlapTile(SpriteKind.Player, assets.tile`PHFSpike`, function (sprite, location) {
     basics.make_sprite_jump(sprite, 190)
     MoveAbility = false
