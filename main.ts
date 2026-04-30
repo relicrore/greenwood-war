097a19806a12b011c3fc64268bb888f6e060fe26
EugeneRedwing8 <emersonejohnson@gmail.com>
2026-04-30 07:59:55 -0700

    Restore '4/27/2026, 11:52:31 AM 1.6.0'

--- main.ts
+++ main.ts
@@ -169,7 +169,7 @@ color.pauseUntilFadeDone()
 color.setPalette(
 color.originalPalette
 )
-Lvl = 6 //blockSettings.readNumber("Lvl")
+Lvl = blockSettings.readNumber("Lvl")
 SongStopped = false
 scroller.setLayerImage(scroller.BackgroundLayer.Layer0, assets.image`TitleLayer0`)
 scroller.scrollBackgroundWithSpeed(-10, 0, scroller.BackgroundLayer.Layer0)
